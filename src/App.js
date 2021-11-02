045b7ccc93f4b0462127332e4366f16f01546669
carolina-rose <karolinagawrzydek@gmail.com>
2021-11-02 21:01:48 +0100

    initial routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route, Redirect} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-     <p>Hello!</p>
-    </div>
+    <Switch>
+      <Route exact path="/"><Redirect path="/homepage" /></Route>
+      <Route path="/homepage">Strona Główna</Route>
+      <Route path="/about">O mnie</Route>
+      <Route path="/projects">Projekty</Route>
+      <Route path="/contact">Kontakt</Route>
+      <Route path="*">Nie znaleziono strony</Route>
+    </Switch>
   );
 }
 
